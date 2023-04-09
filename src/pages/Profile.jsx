3e6725f7782571f32529e1d7e43f4b5e8147dd01
jorlar <jqern.larsen@gmail.com>
2023-04-09 16:47:15 +0200

    :art: Create LogoutButton

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { getAuth } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 const Profile = () => {
+  const auth = getAuth();
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button className='logOut' type='button' onClick={onLogout}>
+          Log Out
+        </button>
+      </header>
     </div>
   );
 };
