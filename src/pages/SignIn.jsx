0c8b01270dd7da10960f13e6bd53a40042a8f8eb
jorlar <jqern.larsen@gmail.com>
2023-04-09 18:59:03 +0200

    :sparkles: Add Google OAuth to project

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,6 +5,7 @@ import { toast } from "react-toastify";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -83,7 +84,7 @@ const SignIn = () => {
             </button>
           </div>
         </form>
-        {/* Google Oauth */}
+        <OAuth />
 
         <Link to='/sign-up' className='registerLink'>
           Sign Up instead
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -11,6 +11,7 @@ import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -109,7 +110,7 @@ const SignUp = () => {
             </button>
           </div>
         </form>
-        {/* Google Oauth */}
+        <OAuth />
 
         <Link to='/sign-in' className='registerLink'>
           Sign In instead
