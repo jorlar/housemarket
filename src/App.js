cd75a860be94b98e063edc07bc0d66ab5f998376
jorlar <jqern.larsen@gmail.com>
2023-04-09 14:17:50 +0200

    :sparkles: Add pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sing-up' element={<SignUp />} />
+          <Route path='/forgott-password' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
