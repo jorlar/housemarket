7a10fc748adb72c06e311ef636dac280dddfb659
jorlar <jqern.larsen@gmail.com>
2023-04-09 14:44:16 +0200

    :art: Style navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import SignUp from "./pages/SignUp";
@@ -19,6 +20,7 @@ function App() {
           <Route path='/sing-up' element={<SignUp />} />
           <Route path='/forgott-password' element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
     </>
   );
